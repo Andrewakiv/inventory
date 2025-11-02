d96d02267d11330ec2a6004364805db77dc2da01
Andrewakiv <danchyshy.andriy@gmail.com>
2025-11-02 17:33:30 +0200

    feat: forms

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_material_view
+from .views import add_material_view, materials_view
 
 
 app_name = 'inventory'
 
 urlpatterns = [
-    path('add-material/', add_material_view, name='add_material')
+    path('add-material/', add_material_view, name='add_material'),
+    path('materials/', materials_view, name='materials')
 ]
--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .forms import MaterialForm
+from .models import Material
 
+
+def materials_view(request):
+    materials = Material.objects.all()
+    return render(request, 'inventory/materials.html', {'materials': materials})
 
 def add_material_view(request):
     form = MaterialForm()
+    if request.method == "POST":
+        form = MaterialForm(request.POST)
+        if form.is_valid():
+            material = form.save(commit=False)
+            material.save()
+            messages.success(request, "Material has been added")
+            return redirect("inventory:materials")
     return render(request, 'inventory/add_material.html', {'form': form})
